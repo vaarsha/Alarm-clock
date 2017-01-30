271837253339e9128538fd5d4ca6c190ba0c4a7c
Varsha Rao <varshar186@gmail.com>
2017-01-30 10:02:22 +0530

    Added comments

--- alrm.py
+++ alrm.py
@@ -1,13 +1,13 @@
 import thread
 import threading
 
-def timeout(timeout=20.0):
+def timeout(timeout=30.0):
     timer = threading.Timer(timeout, thread.interrupt_main)
     astring = None
     try:
         timer.start()
         astring = raw_input("If awake enter stop to shut the alarm\n")
-    except KeyboardInterrupt:
+    except KeyboardInterrupt: #on keyboard interrupt also exit possible
         pass
     timer.cancel()
     return astring
--- clock.py
+++ clock.py
@@ -10,7 +10,7 @@ def alarm():
     print "Hey, You woke up finally"
     return
  
-print "Alarm Clock".center(80)+"\n"
+print "Alarm Clock".center(80)+"\n"  #prints text in center of the screen
 while True:
     hr = int(raw_input("Enter the hour you want to receive notification: "))
     mn = int(raw_input("Enter the minute you want to receive notification: "))
@@ -19,18 +19,18 @@ while True:
     ipt = raw_input("Enter 'n' to reset values otherwise 'y' to continue\n")
     if ipt == 'y':
         break
-time1 = timedelta(hours=hr,minutes=mn)
-reqdtime = datetime.now().time()
-time2 = timedelta(hours = reqdtime.hour,minutes = reqdtime.minute,seconds = reqdtime.second)
-if time2 > time1:
+time1 = timedelta(hours=hr,minutes=mn) #Can do arithmetic operations with it
+reqdtime = datetime.now().time() #Take value as current time, it is datetime object
+time2 = timedelta(hours = reqdtime.hour,minutes = reqdtime.minute,seconds = reqdtime.second) 
+if time2 > time1: #case for if time less than current time
     print "Time has past you cannot set alarm for it!!"
-elif timedelta(hours = reqdtime.hour) == timedelta(hours = hr):
+elif timedelta(hours = reqdtime.hour) == timedelta(hours = hr): #within the same hour
     while timedelta(minutes = reqdtime.minute) != timedelta(minutes = mn):
         left = timedelta(minutes = mn)-timedelta(minutes = reqdtime.minute, seconds = reqdtime.second)
         reqdtime = datetime.now().time()
-    alarm()
+    alarm() #notifies
 else:
     while time2!=time1:
         left = timedelta(hours = hr,minutes = mn)-timedelta(hours = reqdtime.hour,minutes = reqdtime.minute)
         reqdtime = datetime.now().time()
-    alarm()
+    alarm() #notifies
