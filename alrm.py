1ce02b5da345d98c86f82cf7332621631eb8fe0f
Varsha Rao <varshar186@gmail.com>
2017-01-30 09:26:14 +0530

    updated

--- alrm.py
+++ alrm.py
@@ -1,25 +1,24 @@
 import thread
 import threading
 
-def timeout(prompt, timeout=20.0):
-    print "Alarm"  
+def timeout(timeout=20.0):
     timer = threading.Timer(timeout, thread.interrupt_main)
     astring = None
     try:
         timer.start()
-        while True:
-            print "\a"
-            astring = raw_input(prompt)
+        astring = raw_input("If awake enter stop to shut the alarm\n")
     except KeyboardInterrupt:
         pass
     timer.cancel()
     return astring
+
+print "Alarm"
 val = ""
 while True:
-    val = timeout("If awake enter stop to shut the alarm\n")
+    val = timeout()
     if val == "stop":
         break;
 
-print val
+print "Hey, You woke up finally"
         
 
